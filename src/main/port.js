1edd0b728f50d8eb4be43aacff5195d399f19cea
junhuanchen <junhuanchen@qq.com>
2020-02-05 17:42:42 +0800

    add serial write and read delimiter(\n)

---
+++ src/main/port.js
@@ -0,0 +1,24 @@
+// import { ipcMain } from 'electron'
+// const SerialPort = require('serialport')
+
+// // 定义类
+// class LowLevel {
+//   // 构造函数
+//   constructor(x, y) {
+//     this.x = x // 类中变量
+//     this.y = y
+//   }
+//   // 类中函数
+//   toString() {
+//     return '(' + this.x + ', ' + this.y + ')'
+//   }
+//   // 静态函数
+//   static sayHello(name) {
+//     // 修改静态变量
+//     this.para = name
+//     return 'Hello, ' + name
+//   }
+// }
+// // 静态变量
+// Point.para = 'Allen'
+// module.exports = Point
--- src/main/serial_ipc.js
+++ src/main/serial_ipc.js
@@ -10,6 +10,59 @@ ipcMain.on('asynchronous-message', function(event, arg) {
   SerialPort.list((err, ports) => {
     console.log(err, 'ports', ports, JSON.stringify(ports))
     event.sender.send('asynchronous-reply', JSON.stringify(ports))
+
+    const port = new SerialPort(ports[0]['comName'], { autoOpen: false })
+
+    // Open errors will be emitted as an error event
+    port.on('error', function(err) {
+      event.sender.send('asynchronous-reply', err.message)
+      console.log('Error: ', err.message)
+    })
+
+    // port.on('readable', function() {
+    //   console.log('Data:', port.read())
+    // })
+
+    const parser = new SerialPort.parsers.Readline({
+      delimiter: ';'
+    })
+
+    port.pipe(parser)
+
+    parser.on('data', function(data) {
+      console.log('Data:', data.toString())
+      event.sender.send('asynchronous-reply', data.toString())
+    })
+
+    setInterval(function(arg) {
+      console.log(`arg was => ${arg}`, port.isOpen)
+      if (!port.isOpen) {
+        event.sender.send('asynchronous-reply', 'connecting serialport')
+        port.open(function(err) {
+          if (err) {
+            event.sender.send('asynchronous-reply', err.message)
+            return console.log('Error opening port: ', err.message)
+          }
+
+          // Because there's no callback to write, write errors will be emitted on the port:
+          port.write('main screen turn on;')
+        })
+      } else {
+        port.write(Date.now().toString() + ';', function(err) {
+          if (err) {
+            return console.log('Error on write: ', err.message)
+          }
+          console.log('message written')
+        })
+      }
+    }, 1000, 'funky')
+
+    port.write('main screen;', function(err) {
+      if (err) {
+        return console.log('Error on write: ', err.message)
+      }
+      console.log('message written')
+    })
   })
 })
 
