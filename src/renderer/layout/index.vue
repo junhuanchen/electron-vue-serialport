dd337fe317425e31400e5acb9aac4c17c6518442
junhuanchen <junhuanchen@qq.com>
2020-02-18 17:07:55 +0800

    add beautify scroll bar (<el-scrollbar style="height:100%">)

--- src/renderer/layout/index.vue
+++ src/renderer/layout/index.vue
@@ -2,16 +2,18 @@
   <div :class="classObj" class="app-wrapper">
     <div v-if="device==='mobile'&&sidebar.opened" class="drawer-bg" @click="handleClickOutside" />
     <sidebar class="sidebar-container" />
-    <div :class="{hasTagsView:needTagsView}" class="main-container">
-      <div :class="{'fixed-header':fixedHeader}">
-        <navbar />
-        <tags-view v-if="needTagsView" />
+    <el-scrollbar style="height:100%">
+      <div :class="{hasTagsView:needTagsView}" class="main-container">
+        <div :class="{'fixed-header':fixedHeader}">
+          <navbar />
+          <tags-view v-if="needTagsView" />
+        </div>
+        <app-main />
+        <right-panel v-if="showSettings">
+          <settings />
+        </right-panel>
       </div>
-      <app-main />
-      <right-panel v-if="showSettings">
-        <settings />
-      </right-panel>
-    </div>
+    </el-scrollbar>
   </div>
 </template>
 
